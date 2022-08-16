1c382ceeb9bd1ee2dae381cbe118332a6ad6b32b
HamnYce <hamadboa@gmail.com>
2022-08-16 17:07:55 +0300

    add option for exact matching, add feedback for parsing each subreddit

--- lib/json_scraper.rb
+++ lib/json_scraper.rb
@@ -5,12 +5,13 @@ require_relative 'redditparty'
 
 # get the subreddit jsons according to the fuzzy finger results
 class JsonScraper
-  def self.search(query_term, subreddit_amount, safety)
+  def self.search(query_term, subreddit_amount, safety, exact)
     Dir.mkdir "top_listings/#{query_term}/" unless Dir.exist? "top_listings/#{query_term}/"
 
-    subreddits = fuzzy_query(query_term, safety)
+    subreddits = fuzzy_query(query_term, safety, exact)
 
     subreddits.split[0..subreddit_amount - 1].each do |subreddit|
+      puts "fetching from #{subreddit}"
 
       json = RedditParty.subreddit_listing("/#{subreddit}/top/?t=all")
 
@@ -21,7 +22,7 @@ class JsonScraper
     subreddits.split[0..subreddit_amount - 1].count
   end
 
-  def self.fuzzy_query(query_term, safety)
-    `cat subreddit_lists/#{safety}/full_list.txt | fzf --filter="#{query_term}" --algo=v2 -e`
+  def self.fuzzy_query(query_term, safety, exact)
+    `cat subreddit_lists/#{safety}/full_list.txt | fzf --filter="#{query_term}" --algo=v2 #{exact}`
   end
 end
